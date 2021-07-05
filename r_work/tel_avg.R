d12a21a188d6aa029cf4ed5e76dc3e0d9614d163
Shriram <alekhshriram@gmail.com>
2021-07-05 20:16:38 +0530

    tel_av.R

--- r_work/tel_avg.R
+++ r_work/tel_avg.R
@@ -1,12 +1,29 @@
-#Cleaning up
+library(tidyverse)
+
+#load telseq results and isotype list data as tibbles
+tel <- read.csv("telseq_results.tsv",sep="\t")
+tel <-as_tibble(tel)
+isotype <-read.csv("20210115_isotype_assignment.tsv",sep="\t")
+isotype <- as_tibble(isotype)
+
+#Cleaning up tel
 tel <- tel %>% mutate(strain_new = str_replace(strain, ".bam", ""))
+telfinal <- tel %>% rename(length_estimate=LENGTH_ESTIMATE)
 
-#rename a column
-tel <- tel %>% rename(length_estimate=LENGTH_ESTIMATE)
-
-#Combine two tibbles with a common column
+#Combine the two tibbles with a common column
 telfinal %>% inner_join(isotype)
 
-
 #Find the mean length for each isotype
-new_tel<-tel %>% group_by(isotype) %>% summarise(mean_length = mean(length_estimate))
+new_tel<-telfinal %>% group_by(isotype) %>% summarise(mean_length = mean(length_estimate))
+
+
+#Rescaling to celegans length from human length
+#human c value : (332720800/46000)
+#celegans c value : ((5808700/12000))
+new_tel %>% mutate(celegans_val=(mean_length/(332720800/46000)) * (5808700/12000))
+
+#round the mean length
+final <- new_tel %>% mutate(celegans_val_rounded=round(celegans_val,0))
+
+#Keep only the necessary columns
+final_stripped <- final %>% select(isotype, celegans_val_rounded)
