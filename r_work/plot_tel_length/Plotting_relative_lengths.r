dfb89dcf6c7e8a677ea7624ac34b6e9e7d97082d
Shriram <alekhshriram@gmail.com>
2021-08-20 12:59:34 -0500

    briggsae isotype-strain

--- r_work/plot_tel_length/Plotting_relative_lengths.r
+++ r_work/plot_tel_length/Plotting_relative_lengths.r
@@ -14,3 +14,21 @@
 > view(Lengths)
 > ggplot(Lengths,aes(length,fill=worm))+
 +     geom_histogram(alpha=0.5,position='identity')
+
+########
+#Individual lengths
+########
+
+#brig
+brig_graph <- ggplot(briggsae_length,aes(brigg_length_estimate))+geom_histogram(bins=20)
+> ggplot(briggsae_length,aes(brigg_length_estimate))+geom_histogram(bins=20)
+> ggplot(briggsae_length,aes(brigg_length_estimate))+geom_histogram(bins=25)
+> ggplot(briggsae_length,aes(brigg_length_estimate))+geom_histogram(bins=20)
+> brig_graph <- brig_graph+ggtitle("briggsae")+xlab("length")
+> brig_graph
+
+#eleg
+> eleg_graph <- ggplot(elegans_length,aes(avg_telomere_length))+geom_histogram(bins=40) + ggtitle("elegans")+xlab("length")
+
+#trop
+> trop_graph <- ggplot(tropicalis_length,aes(length_estimate))+geom_histogram(bins=24) + ggtitle("tropicalis")+xlab("length")
